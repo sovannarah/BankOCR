c7c73979165efae7b847334524a0c3ddb552a51f
sova <sovannara.hem@sayse.fr>
2021-11-13 14:45:02 +0100

    [func]: reading text file by the server

--- server/routes/file.js
+++ server/routes/file.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
+router.get('/read', async (req, res, next) => {
+    fs.readFile('./numbers.txt', 'utf8' , (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+        console.log(data)
 
+    })
+})
 
 module.exports = router;
 
