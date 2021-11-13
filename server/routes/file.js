fe0dac21f44d055cf9fd142ac597cf2a16c66b22
sova <sovannara.hem@sayse.fr>
2021-11-13 18:04:22 +0100

    [func]: upload file

--- server/routes/file.js
+++ server/routes/file.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const mv = require('mv')
+const formidable = require('formidable');
 const {scansFile} = require('../utils/scans');
 
-router.get('/read', async (req, res, next) => {
-    fs.readFile('./numbers.txt', 'utf8' , (err, data) => {
-        if (err) {
-            console.error(err);
+router.post('/read', async (req, res, next) => {
+
+
+    let form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+        // let filename = files.document.name.replace(/\s/g, "");
+        let oldPath = files.file.filepath;
+        let newPath = './numbers.txt';
+        if (fs.existsSync(newPath)) {
+            fs.unlink(newPath, err => {
+                if (err) throw err;
+                // console.log('File deleted!')
+
+            });
         }
-        res.send(scansFile(data));
+            mv(oldPath, newPath, {mkdirp: true}, err => {
+                if (!err) {
+                    fs.readFile(newPath, 'utf8', (err, data) => {
+                        if (err) {
+                            console.error(err);
+                        }
+                        res.send(scansFile(data));
+                    })
+                }
+            })
     })
+
+
+    // res.send(scansFile(data));
 })
 
 module.exports = router;
--- src/App.js
+++ src/App.js
@@ -8,22 +8,47 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            accountNumbers: []
+            accountNumbers: [],
+            file: null
         }
     }
 
     componentDidMount() {
-        axios.get('http://localhost:4245/file/read')
+        // axios.get('http://localhost:4245/file/read')
+        //     .then(res => {
+        //         this.setState({accountNumbers: res.data})
+        //         console.log(res.data)
+        //     })
+    }
+
+    onFileChange = event => {
+        this.setState({ file: event.target.files[0] });
+    };
+
+    upload = () => {
+        const {file} = this.state;
+        const formData = new FormData();
+
+        formData.append(
+            "file",
+            file,
+            file.name
+        );
+
+        axios.post('http://localhost:4245/file/read', formData)
             .then(res => {
                 this.setState({accountNumbers: res.data})
                 console.log(res.data)
             })
+
     }
 
     render() {
         const {accountNumbers} = this.state;
         return (
             <div className="App">
+                <input type="file" onChange={this.onFileChange} />
+                <button onClick={this.upload}>Upload</button>
                 <Accounts accountNumbers={accountNumbers}/>
             </div>
         )
