72b7c341dd2ea68efa3aa77074c54ff7f8c980e6
sova <sovannara.hem@sayse.fr>
2021-11-13 15:13:53 +0100

    [func]: refactor content file

--- server/utils/scans.js
+++ server/utils/scans.js
@@ -1,7 +1,35 @@
+const digitHeight = 4;
+const digitWidth = 3;
+const numberOfDigit = 9;
+const numberOfColumn = digitWidth * numberOfDigit;
+console.log(numberOfColumn)
 
+function refactorContentFile(contentFile) {
+    const strToArrFile = `${contentFile}`.split("\r\n");
+    let newStr = '';
+    strToArrFile.forEach((line, index) => {
+        const blankSpace = ' ';
+        if((index > 0 && (index + 1) % 4 === 0) || line === '') {
+            newStr += blankSpace.repeat(numberOfColumn);
+        } else if (line.length < numberOfColumn) {
+            newStr += line + blankSpace.repeat(numberOfColumn - line.length);
+        } else if (line.length > numberOfColumn) {
+            newStr += line.substr(0, numberOfColumn);
+        } else {
+            newStr +=  line;
+        }
+    })
+    const nbLines = newStr.length / numberOfColumn;
+    const nbNumberLines = nbLines / 4;
+    if(newStr.length % numberOfColumn === 0 && nbNumberLines % 1 === 0) {
+        return newStr;
+    }
+    return 'ERROR FILE';
+}
 
 function scansFile(contentFile) {
-    console.log(contentFile);
+    const refactorData = refactorContentFile(contentFile);
+    console.log(refactorData)
 }
 
 module.exports = {scansFile};
